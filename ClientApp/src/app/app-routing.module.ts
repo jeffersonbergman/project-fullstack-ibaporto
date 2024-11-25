bf1bb437cc3d80b91f496969471d7eabf49a8a7f
jeffersonbergman <jefferson.bergman@yahoo.com>
2024-11-25 13:33:12 +0000

    changes in routing

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { IbaLoginComponent } from './components/iba-login/iba-login.component';
 import { IbaHomeComponent } from './components/iba-home/iba-home.component';
 import { IbaContactComponent } from './components/iba-contact/iba-contact.component';
 import { IbaMinistriesComponent } from './components/iba-ministries/iba-ministries.component';
+import {IbaAboutComponent} from "./components/iba-about/iba-about.component";
 
 const routes: Routes = [
   {path: '', component: IbaHomeComponent},
   {path: 'login', component: IbaLoginComponent},
   {path: 'contact', component: IbaContactComponent},
-  {path: 'ministries', component: IbaMinistriesComponent}
+  {path: 'ministries', component: IbaMinistriesComponent},
+  {path: 'about', component: IbaAboutComponent}
 ];
 
 @NgModule({
